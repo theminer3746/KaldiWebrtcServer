b5017d9d416e0dac8e052d8e05d576b92073661f
theminer3746 <me@theminerdev.com>
2021-05-06 00:38:06 +0700

    Revert back to use online2-tcp-nnet3-decode-faster

--- docker/model/run.sh
+++ docker/model/run.sh
@@ -7,12 +7,13 @@ export PATH=$KALDI_ROOT/src/bin:$KALDI_ROOT/tools/openfst/bin:$KALDI_ROOT/src/fs
 while :
 do
   # loop infinitely
-  online-audio-server-decode-faster \
+  online-server-gmm-decode-faster \
     --verbose=1 --rt-min=0.5 --rt-max=3.0 \
     --max-active=6000 --beam=13.0 \
     --acoustic-scale=0.08333 \
     tri2/final.mdl tri2/graph/HCLG.fst \
-    tri2/graph/words.txt '1' \
-    tri2/graph/phones/word_boundary.int 5010 
+    tri2/graph/words.txt '1' 5050
+    # tri2/graph/phones/word_boundary.int \
+    # 5010 
     #\ tri2/final.mdl
 done
